ce69e66b495da056021ff416513d3fd63c9f4205
JJ Rosenberg <jjrosenberg122@gmail.com>
2024-04-15 23:50:16 +0200

    Updated adCreator

---
+++ Assets/AdCreatorTool.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class AdCreatorTool : MonoBehaviour
+{
+    public GameObject adPrefab;
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
+    public Orientation orientation = Orientation.Horizontal;
+    public float minLength = 0.1f;  
+    public float maxLength = 0.5f; 
+    public float minWidth = 0.1f; 
+    public float maxWidth = 0.5f;   
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(2)) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                SpawnPrefab(hit.point, hit.normal);
+            }
+        }
+    }
+
+    void SpawnPrefab(Vector3 position, Vector3 normal)
+    {
+        Vector3 toCamera = (Camera.main.transform.position - position).normalized;
+        Vector3 forward = -Vector3.Cross(normal, Vector3.Cross(toCamera, normal)).normalized;
+        Quaternion lookAtCamera = Quaternion.LookRotation(forward, normal);
+
+        lookAtCamera = orientation == Orientation.Vertical
+            ? Quaternion.LookRotation(forward, Vector3.Cross(forward, normal))
+            : lookAtCamera;
+
+        float xRotation = orientation == Orientation.Horizontal ? 0 : 90;
+        Quaternion finalRotation = Quaternion.Euler(xRotation, lookAtCamera.eulerAngles.y, lookAtCamera.eulerAngles.z);
+
+        Vector3 adjustedPosition = position + (toCamera * 0.1f);
+
+        GameObject spawnedAd = GameObject.Instantiate(adPrefab, adjustedPosition, finalRotation);
+        Vector3 scale = new Vector3(Random.Range(minWidth, maxWidth), 1.0f, Random.Range(minLength, maxLength));
+        spawnedAd.transform.localScale = scale;
+    }
+}
