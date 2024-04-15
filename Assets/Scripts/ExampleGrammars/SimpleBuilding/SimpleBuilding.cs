a6be8789cb8c51637b0ecd3b5d15ffd97c454159
JJ Rosenberg <jjrosenberg122@gmail.com>
2024-04-15 15:20:06 +0200

    Custom building

--- Assets/Scripts/ExampleGrammars/SimpleBuilding/SimpleBuilding.cs
+++ Assets/Scripts/ExampleGrammars/SimpleBuilding/SimpleBuilding.cs
@@ -1,60 +1,55 @@
 ﻿using UnityEngine;
 
-namespace Demo {
-	public class SimpleBuilding : Shape {
-		public int buildingHeight=-1; // The total building height (=#stocks) - value should be the same for all stocks
-		public float stockHeight=1; // The height of one stock. Change this value depending on the height of your stock prefabs
-		// If buildingHeight is negative, a random building height will be chosen between these two limits:
-		public int maxHeight=5;  
-		public int minHeight=1;
+namespace Demo
+{
+    public class SimpleBuilding : Shape
+    {
+        public int buildingHeight = -1;
+        public float stockHeight = 1;
+        public int maxHeight = 5;
+        public int minHeight = 1;
 
-		public GameObject[] floorPrefabs;
-		public GameObject[] stockPrefabs; // Your stock prefabs (make sure they all have the same height)
-		public GameObject[] roofPrefabs; // Your roof prefabs (may have different height)
+        public GameObject[] floorPrefabs;
+        public GameObject[] stockPrefabs;
+        public GameObject[] roofPrefabs;
 
-		int stockNumber = 0; // The number of stocks that have already been spawned below this
+        int stockNumber = 0;
 
-		public void Initialize(int pBuildingHeight, float pStockHeight, int pStockNumber, GameObject[] pStockPrefabs, GameObject[] pRoofPrefabs) {
-			buildingHeight = pBuildingHeight;
-			stockHeight = pStockHeight;
-			stockNumber = pStockNumber;
-			stockPrefabs = pStockPrefabs;
-			roofPrefabs = pRoofPrefabs;
-		}
+        public void Initialize(int pBuildingHeight, float pStockHeight, int pStockNumber, GameObject[] pStockPrefabs, GameObject[] pRoofPrefabs, int pMinHeight, int pMaxHeight)
+        {
+            buildingHeight = pBuildingHeight;
+            stockHeight = pStockHeight;
+            stockNumber = pStockNumber;
+            stockPrefabs = pStockPrefabs;
+            roofPrefabs = pRoofPrefabs;
+            minHeight = pMinHeight;
+            maxHeight = pMaxHeight;
+        }
 
-		// Returns a random game object chosen from a given gameobject array
-		GameObject ChooseRandom(GameObject[] choices) {
-			int index = Random.Range(0, choices.Length);
-			return choices[index];
-		}
+        GameObject ChooseRandom(GameObject[] choices)
+        {
+            int index = Random.Range(0, choices.Length);
+            return choices[index];
+        }
 
-		protected override void Execute() {
-			if (buildingHeight<0) { // This is only done once for the root symbol
-				buildingHeight = Random.Range(minHeight, maxHeight+1);
-			}
+        protected override void Execute()
+        {
+            if (buildingHeight < 0)
+            {
+                buildingHeight = Random.Range(minHeight, maxHeight + 1);
+            }
 
-			if (stockNumber<buildingHeight) {
-				// First spawn a new stock...
-				GameObject newStock;
-				if(buildingHeight < 1)
-                {
-					newStock = SpawnPrefab(floorPrefabs[0]);
-					return;
-                }
-				newStock = SpawnPrefab(ChooseRandom(stockPrefabs));
-
-				//  ...and then continue with the remainder of the building, right above the spawned stock:
-
-				// Create a new symbol - make sure to increase the y-coordinate:
-				SimpleBuilding remainingBuilding = CreateSymbol<SimpleBuilding>("stock", new Vector3(0, stockHeight, 0));
-				// Pass the parameters to the new symbol (component), but increase the stockNumber:
-				remainingBuilding.Initialize(buildingHeight, stockHeight, stockNumber+1, stockPrefabs, roofPrefabs);
-				// ...and continue with the shape grammar:
-				remainingBuilding.Generate(buildDelay);
-			} else {
-				// Spawn a roof and stop:
-				GameObject newRoof = SpawnPrefab(ChooseRandom(roofPrefabs));
-			}
-		}
-	}
+            if (stockNumber < buildingHeight)
+            {
+                GameObject newStock = buildingHeight == 1 ? SpawnPrefab(floorPrefabs[0]) : SpawnPrefab(ChooseRandom(stockPrefabs));
+                SimpleBuilding remainingBuilding = CreateSymbol<SimpleBuilding>("stock", new Vector3(0, stockHeight, 0));
+                remainingBuilding.Initialize(buildingHeight, stockHeight, stockNumber + 1, stockPrefabs, roofPrefabs, minHeight, maxHeight);
+                remainingBuilding.Generate(buildDelay);
+            }
+            else if (stockNumber >= buildingHeight)
+            {
+                GameObject newRoof = SpawnPrefab(ChooseRandom(roofPrefabs));
+            }
+        }
+    }
 }
